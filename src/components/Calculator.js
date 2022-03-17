16f4a9a13d75e93cc570909cee5bb1aa689357d4
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-03-16 22:04:13 -0500

    Add functionality

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,39 +1,84 @@
 import React from 'react';
 import Key from './InputCalc';
+import calculate from './logic/calculate';
 
 class Calculator extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.retrieveSymbol = this.retrieveSymbol.bind(this);
+    this.state = {
+      display: '0',
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  async retrieveSymbol(symbol) {
+    const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+    const operators = ['%', '÷', 'x', '-', '+'];
+    const {
+      display, total, next, operation,
+    } = this.state;
+
+    console.log(symbol);
+
+    if (numbers.includes(symbol)) {
+      if (display === '0') {
+        this.setState({ display: symbol, total: symbol });
+      } else if (total === null) {
+        this.setState({ display: display + symbol, total: symbol });
+      } else { this.setState({ display: display + symbol, total: total + symbol }); }
+    } else if (operators.includes(symbol)) {
+      this.setState({
+        operation: symbol,
+        next: total,
+        total: null,
+        display: `${display} ${symbol} `,
+      });
+    } else if (symbol === '=') {
+      this.setState(calculate({ total, next, operation }, symbol));
+      this.setState({ display: total });
+    } else {
+      this.setState(calculate({ total, next, operation }, symbol));
+      this.setState({ display: total });
+    }
+    console.log(this.state);
+  }
+
   render() {
+    const { display } = this.state;
     return (
       <div>
-        <div className="screen">0</div>
+        <div className="screen">{display}</div>
         <div className="row">
-          <Key symbol="AC" />
-          <Key symbol="+/-" />
-          <Key symbol="%" />
-          <Key symbol="÷" color="orange" />
+          <Key symbol="AC" retrieveSymbol={this.retrieveSymbol} />
+          <Key symbol="+/-" retrieveSymbol={this.retrieveSymbol} />
+          <Key symbol="%" retrieveSymbol={this.retrieveSymbol} />
+          <Key symbol="÷" color="orange" retrieveSymbol={this.retrieveSymbol} />
         </div>
         <div className="row">
-          <Key symbol="7" />
-          <Key symbol="8" />
-          <Key symbol="9" />
-          <Key symbol="X" color="orange" />
+          <Key symbol="7" retrieveSymbol={this.retrieveSymbol} />
+          <Key symbol="8" retrieveSymbol={this.retrieveSymbol} />
+          <Key symbol="9" retrieveSymbol={this.retrieveSymbol} />
+          <Key symbol="x" color="orange" retrieveSymbol={this.retrieveSymbol} />
         </div>
         <div className="row">
-          <Key symbol="4" />
-          <Key symbol="5" />
-          <Key symbol="6" />
-          <Key symbol="-" color="orange" />
+          <Key symbol="4" retrieveSymbol={this.retrieveSymbol} />
+          <Key symbol="5" retrieveSymbol={this.retrieveSymbol} />
+          <Key symbol="6" retrieveSymbol={this.retrieveSymbol} />
+          <Key symbol="-" color="orange" retrieveSymbol={this.retrieveSymbol} />
         </div>
         <div className="row">
-          <Key symbol="1" />
-          <Key symbol="2" />
-          <Key symbol="3" />
-          <Key symbol="+" color="orange" />
+          <Key symbol="1" retrieveSymbol={this.retrieveSymbol} />
+          <Key symbol="2" retrieveSymbol={this.retrieveSymbol} />
+          <Key symbol="3" retrieveSymbol={this.retrieveSymbol} />
+          <Key symbol="+" color="orange" retrieveSymbol={this.retrieveSymbol} />
         </div>
         <div className="row">
-          <Key symbol="0" color="double" />
-          <Key symbol="." />
-          <Key symbol="=" color="orange" />
+          <Key symbol="0" color="double" retrieveSymbol={this.retrieveSymbol} />
+          <Key symbol="." retrieveSymbol={this.retrieveSymbol} />
+          <Key symbol="=" color="orange" retrieveSymbol={this.retrieveSymbol} />
         </div>
       </div>
     );
--- src/components/InputCalc.js
+++ src/components/InputCalc.js
@@ -2,11 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Key extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.retrieveSymbol = this.retrieveSymbol.bind(this);
+  }
+
+  retrieveSymbol(e) {
+    this.props.retrieveSymbol(e.target.innerText);
+  }
+
   render() {
     const { color, symbol } = this.props;
     return (
       <div>
-        <button type="button" className={color}>
+        <button type="button" className={color} onClick={this.retrieveSymbol}>
           {symbol}
         </button>
       </div>
