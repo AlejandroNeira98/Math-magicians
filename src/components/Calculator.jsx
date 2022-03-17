5f6cd1ca140e25a9a0b354aac7e48c8ff7e2b8ef
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-03-17 17:41:19 -0500

    Refactor Class

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,77 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Key from './InputCalc';
 import calculate from './logic/calculate';
 
-class Calculator extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.retrieveSymbol = this.retrieveSymbol.bind(this);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-  }
+const Calculator = () => {
+  const [total, setTotal] = useState(null);
+  const [next, setNext] = useState(null);
+  const [operation, setOperation] = useState(null);
+  const [display, setDisplay] = useState('0');
 
-  retrieveSymbol(symbol) {
-    const {
-      total, next, operation,
-    } = this.state;
+  const retrieveSymbol = (symbol) => {
     console.log(symbol);
 
-    this.setState(calculate({ total, next, operation }, symbol));
-    this.setState((prevState) => ({
-      total: prevState.total,
-      next: prevState.next,
-      operation: prevState.operation,
-    }));
+    const calcObj = calculate({ total, next, operation }, symbol);
+    setTotal(calcObj.total);
+    setNext(calcObj.next);
+    setOperation(calcObj.operation);
 
-    console.log(this.state);
-  }
+    if (!next && !total && !operation) { setDisplay('0'); }
+    if (next && !total && !operation) { setDisplay(next); }
+    if (!next && total && operation) { setDisplay('0'); }
+    if (next && total && operation) { setDisplay(next); }
+    if (!next && total && !operation) { setDisplay(total); }
 
-  render() {
-    let display = '0';
-    const { next, total, operation } = this.state;
-    if (next === null && total === null && operation === null) { display = '0'; }
-    if (next !== null && total === null && operation === null) { display = next; }
-    if (next === null && total !== null && operation !== null) { display = '0'; }
-    if (next !== null && total !== null && operation !== null) { display = next; }
-    if (next === null && total !== null && operation === null) { display = total; }
-    return (
-      <div>
-        <div className="screen">{display}</div>
-        <div className="row">
-          <Key symbol="AC" retrieveSymbol={this.retrieveSymbol} />
-          <Key symbol="+/-" retrieveSymbol={this.retrieveSymbol} />
-          <Key symbol="%" retrieveSymbol={this.retrieveSymbol} />
-          <Key symbol="÷" color="orange" retrieveSymbol={this.retrieveSymbol} />
-        </div>
-        <div className="row">
-          <Key symbol="7" retrieveSymbol={this.retrieveSymbol} />
-          <Key symbol="8" retrieveSymbol={this.retrieveSymbol} />
-          <Key symbol="9" retrieveSymbol={this.retrieveSymbol} />
-          <Key symbol="x" color="orange" retrieveSymbol={this.retrieveSymbol} />
-        </div>
-        <div className="row">
-          <Key symbol="4" retrieveSymbol={this.retrieveSymbol} />
-          <Key symbol="5" retrieveSymbol={this.retrieveSymbol} />
-          <Key symbol="6" retrieveSymbol={this.retrieveSymbol} />
-          <Key symbol="-" color="orange" retrieveSymbol={this.retrieveSymbol} />
-        </div>
-        <div className="row">
-          <Key symbol="1" retrieveSymbol={this.retrieveSymbol} />
-          <Key symbol="2" retrieveSymbol={this.retrieveSymbol} />
-          <Key symbol="3" retrieveSymbol={this.retrieveSymbol} />
-          <Key symbol="+" color="orange" retrieveSymbol={this.retrieveSymbol} />
-        </div>
-        <div className="row">
-          <Key symbol="0" color="double" retrieveSymbol={this.retrieveSymbol} />
-          <Key symbol="." retrieveSymbol={this.retrieveSymbol} />
-          <Key symbol="=" color="orange" retrieveSymbol={this.retrieveSymbol} />
-        </div>
+    console.log(total, next, operation, display);
+  };
+
+  return (
+    <div>
+      <div className="screen">{display}</div>
+      <div className="row">
+        <Key symbol="AC" retrieveSymbol={retrieveSymbol} />
+        <Key symbol="+/-" retrieveSymbol={retrieveSymbol} />
+        <Key symbol="%" retrieveSymbol={retrieveSymbol} />
+        <Key symbol="÷" color="orange" retrieveSymbol={retrieveSymbol} />
       </div>
-    );
-  }
-}
+      <div className="row">
+        <Key symbol="7" retrieveSymbol={retrieveSymbol} />
+        <Key symbol="8" retrieveSymbol={retrieveSymbol} />
+        <Key symbol="9" retrieveSymbol={retrieveSymbol} />
+        <Key symbol="x" color="orange" retrieveSymbol={retrieveSymbol} />
+      </div>
+      <div className="row">
+        <Key symbol="4" retrieveSymbol={retrieveSymbol} />
+        <Key symbol="5" retrieveSymbol={retrieveSymbol} />
+        <Key symbol="6" retrieveSymbol={retrieveSymbol} />
+        <Key symbol="-" color="orange" retrieveSymbol={retrieveSymbol} />
+      </div>
+      <div className="row">
+        <Key symbol="1" retrieveSymbol={retrieveSymbol} />
+        <Key symbol="2" retrieveSymbol={retrieveSymbol} />
+        <Key symbol="3" retrieveSymbol={retrieveSymbol} />
+        <Key symbol="+" color="orange" retrieveSymbol={retrieveSymbol} />
+      </div>
+      <div className="row">
+        <Key symbol="0" color="double" retrieveSymbol={retrieveSymbol} />
+        <Key symbol="." retrieveSymbol={retrieveSymbol} />
+        <Key symbol="=" color="orange" retrieveSymbol={retrieveSymbol} />
+      </div>
+    </div>
+  );
+};
 
 export default Calculator;
