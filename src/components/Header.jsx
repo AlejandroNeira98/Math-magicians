7ac1390ffc30b858ab3626e7866c0fb156344c47
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-03-29 16:13:02 -0500

    Add Header and routes

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        Math Magicians
+      </h1>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/calculator">Calculator</Link>
+        <Link to="/quote">Quote</Link>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Pages/Quote.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Quote() {
+  return (
+    <div>
+      &quot Mathematics is not about numbers, equations, computations,
+      or algorithms: is about understanding &quot -William Paul Thurston
+    </div>
+  );
+}
+
+export default Quote;
