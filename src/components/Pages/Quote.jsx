b48ff7943388ab9afccaf7f826645c6a832900d7
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-03-29 17:36:09 -0500

    Add styling

--- src/components/Pages/Quote.jsx
+++ src/components/Pages/Quote.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
 function Quote() {
+  const css = { fontSize: '2rem', margin: '10% 10% 0 10%' };
   return (
-    <div>
-      Mathematics is not about numbers, equations, computations,
-      or algorithms: is about understanding -William Paul Thurston
-    </div>
+    <>
+      <div style={css}>
+        Mathematics is not about numbers, equations, computations,
+        or algorithms: is about understanding
+      </div>
+      <div style={css}>
+        -William Paul Thurston
+      </div>
+    </>
   );
 }
 
